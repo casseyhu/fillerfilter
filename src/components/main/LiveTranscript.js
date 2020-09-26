416c718b2e31633d595572902b355fb063124a20
casseyhu <casseyhu123@gmail.com>
2020-09-25 23:52:50 -0400

    more css updates

--- src/components/main/LiveTranscript.js
+++ src/components/main/LiveTranscript.js
@@ -4,9 +4,9 @@ export class LiveTranscript extends Component {
     render() {
         return (
             <div className="live_transcript_box">
-                <header className="live_transcript_header">
-                    Live Transcript
-                </header>
+                <h5 className="live_transcript_header">
+                    LIVE TRANSCRIPT
+                </h5>
                 <div className="transcript">
                     <p>{this.props.transcript}</p>
                 </div>

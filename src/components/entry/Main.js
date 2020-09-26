2f5e6a4b8fcadbb4910b7e74606345c7364dd23a
casseyhu <casseyhu123@gmail.com>
2020-09-26 01:57:38 -0400

    fixed horizontal scroll

--- src/components/entry/Main.js
+++ src/components/entry/Main.js
@@ -3,7 +3,7 @@ import Navbar from '../navbar/Navbar'
 
 class Main extends Component {
     render () {
-        document.body.style = 'width: 100vw; height: 100vh;';
+        document.body.style = 'maxWidth:100%; height: 100vh; overflow: hidden;';
         return (
             <div className="app">
                 <h1 class="centert glitch is-glitching" data-text="fillerfilter">fillerfilter</h1>

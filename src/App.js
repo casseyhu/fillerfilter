7217bb95052ede71b580afb70868bfabc22306a1
Sooyeon Kim <sooyeon.kim.2@stonybrook.edu>
2020-09-26 02:47:41 -0400

    Added Page Transition

--- src/App.js
+++ src/App.js
@@ -1,29 +1,41 @@
 import React, {Component} from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Switch, Route, withRouter } from 'react-router-dom';
 import Entry from './components/entry/Entry'
 import Main from './components/entry/Main'
 import { TransitionGroup, CSSTransition } from "react-transition-group"
 import './css/transitions.scss'
 
 class App extends Component {
+    state={
+        prevDepth: this.getPathDepth(this.props.location)
+    }
+
+    componentWillReceiveProps() {
+        this.setState({ prevDepth: this.getPathDepth(this.props.location) });
+    }
+      
+    getPathDepth(location) {
+        let pathArr = location.pathname.split("/");
+        pathArr = pathArr.filter(n => n !== "");
+        return pathArr.length;
+    }
+
     render() {
-        const {location} = this.props
+        const {location} = this.props;
         return (
-            <BrowserRouter>
-                <TransitionGroup component="div" className="App">
-                    <CSSTransition key="/" timeout={{enter:800, exit:400}} classNames="slider" mountOnEnter={false} unmountOnExit={true}>
-                    <div className="right">
-                        <Switch>
-                            <Route path='/' exact component={Entry} />
-                            <Route path="/home" exact component={Main} />
-                        </Switch>
-                    </div>
-                    </CSSTransition>
-                </TransitionGroup>
-            </BrowserRouter>
+            <TransitionGroup component="div" className="App">
+                <CSSTransition key={location.pathname.split("/")[1] || "/" } timeout={{enter:800, exit:800}} classNames="slider" mountOnEnter={false} unmountOnExit={true}>
+                <div className="right">
+                    <Switch location={location}> 
+                        <Route path='/' exact component={Entry} />
+                        <Route path="/home" exact component={Main} />
+                    </Switch>
+                </div>
+                </CSSTransition>
+            </TransitionGroup>
         );
     }
     
 }
 
-export default App;
+export default withRouter(App);
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/style.css';
 import App from './App';
+import { BrowserRouter } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));

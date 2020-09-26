42ff2aea0ce28dfda2fab14ad5361e1d48124a79
Sooyeon Kim <sooyeon.kim.2@stonybrook.edu>
2020-09-26 14:12:42 -0400

    added logo

--- src/components/entry/Entry.js
+++ src/components/entry/Entry.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react'
 import {Link} from "react-router-dom"
 import '../../css/style.css'
+import logo from "./logotext.png"
 
 class Entry extends Component {
     render () {
         return <div>
             <div className="entry">
             <h1 className="centert glitch is-glitching" data-text="fillerfilter" style={{float:"left"}}>fillerfilter</h1>
+            <img src={logo} alt="logo" width="20%" height="20%" style={{transform: "rotate(-45deg)"}}/>
             <Link to='/home'>
                 <button id="enterbutton" className="btn red waves-effect lighten-1 z-depth-0"> <img src="http://www.elitek911.com/images/right-arrows-2x.png" width="50px" height="50px"></img> </button>
             </Link>
--- src/components/entry/Main.js
+++ src/components/entry/Main.js
@@ -1,12 +1,15 @@
 import React, {Component, useState} from 'react'
 import Navbar from '../navbar/Navbar'
+import logo from './logotext.png'
 
 class Main extends Component {
     render () {
         document.body.style = 'background-color: #B7B7CB; maxWidth:100%; height: 100vh; overflow: hidden;';
         return (
             <div className="app">
+                <div style={{float: "left", paddingLeft: "200px", marginRight: "-400px"/*, transform: "rotate(-45deg)"*/}}><img src={logo} alt="logo" width="100" height="100"/></div>
                 <h1 class="centert glitch is-glitching" data-text="fillerfilter">fillerfilter</h1>
+                <div style={{float: "right", marginTop:"-100px", paddingRight: "200px"/*, transform: "rotate(-45deg)}"*/}}><img src={logo} alt="logo" width="100" height="100"/></div>
                 <Navbar/>
             </div>
         );
